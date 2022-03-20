c86e048c8c4bb041a76c215dd9648d0dbff192d9
Long <philong26102003@gmail.com>
2022-03-20 22:46:55 +0700

    VLong

---
+++ bai4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+ 
+int main()
+{
+	int n;
+	cout<<" Nhap  n: ";
+	cin>>n;
+	cout<<" Bang cuu chuong  "<< n <<":"<<endl;
+	for(int i = 1; i <= 10; i++) 
+	{
+		cout<<"   "<< n <<" x "<< i <<" = "<< n*i <<endl;
+	}
+	return 0;
+}
